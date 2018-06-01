34415425cd50f6c12cbbc3af4a36095da7146d63
Angel Lopez <jxnior21@Angels-MacBook-Pro.local>
2018-06-01 09:58:01 -0400

    Added files for code igniter project

---
+++ application/views/layout/header.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title></title>
+    <meta name="description" content="Description of the page less than 150 characters">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+    <link rel="stylesheet" href=<?php echo base_url('assets/css/main.css'); ?>>
+  </head>
+  <body>
